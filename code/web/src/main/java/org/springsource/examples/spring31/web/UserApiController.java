d490eb4a7a3e7a7f898a3d6fd7d189af20403555
Josh Long <josh@joshlong.com>
2013-01-15 08:19:31 +0800

    fixed a small bug, for now.

--- code/web/src/main/java/org/springsource/examples/spring31/web/UserApiController.java
+++ code/web/src/main/java/org/springsource/examples/spring31/web/UserApiController.java
@@ -12,7 +12,6 @@ import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.concurrent.Callable;
 
 /**
  * Simple Spring MVC controller that can be used to adminster information about the users
@@ -73,22 +72,17 @@ public class UserApiController {
     // more correct
     @RequestMapping(value = USER_COLLECTION_ENTRY_URL + "/photo", method = RequestMethod.POST)
     @ResponseBody
-    public Callable<Long> uploadBasedOnPathVariable(final @PathVariable("userId") Long userId, final @RequestParam("file") MultipartFile file) {
+    public Long uploadBasedOnPathVariable(final @PathVariable("userId") Long userId, final @RequestParam("file") MultipartFile file) {
+        try {
+            assert userId != null : "you must specify a userId when uploading!";
+            assert file != null : "you must specify a file object when uploading!";
+            byte[] bytesForImage = file.getBytes();
+            userService.writeUserProfilePhotoAndQueueForConversion(userId, file.getName(), bytesForImage);
+            return userId;
+        } catch (Throwable th) {
+            throw new RuntimeException("Something happened while uploading the managed upload", th);
+        }
 
-        return new Callable<Long>() {
-            @Override
-            public Long call() throws Exception {
-                try {
-                    assert userId != null : "you must specify a userId when uploading!";
-                    assert file != null : "you must specify a file object when uploading!";
-                    byte[] bytesForImage = file.getBytes();
-                    userService.writeUserProfilePhotoAndQueueForConversion(userId, file.getName(), bytesForImage);
-                    return userId;
-                } catch (Throwable th) {
-                    throw new RuntimeException("Something happened while uploading the managed upload", th);
-                }
-            }
-        };
     }
 
 
