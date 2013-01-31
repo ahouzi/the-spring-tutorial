e9cbaee884d14527998e2c957b29956d3299ccc7
Josh Long <josh@joshlong.com>
2013-01-31 03:10:39 -0800

    creating a simpler "web" module.

--- code/web/src/main/java/org/springsource/examples/spring31/web/config/servlet/CrmWebApplicationInitializer.java
+++ code/web/src/main/java/org/springsource/examples/spring31/web/config/servlet/CrmWebApplicationInitializer.java
@@ -1,17 +1,14 @@
 package org.springsource.examples.spring31.web.config.servlet;
 
-import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.filter.DelegatingFilterProxy;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
-import org.springframework.web.multipart.support.MultipartFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springsource.examples.spring31.web.config.WebMvcConfiguration;
 
-import javax.servlet.*;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 /**
  * Simple replacement for <CODE>web.xml</CODE> that is constructed entirely in Java code.
@@ -28,13 +25,14 @@ public class CrmWebApplicationInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
 
-     //   registerFilter(servletContext, "springSecurityFilterChain", new DelegatingFilterProxy());
-        registerFilter(servletContext, "hiddenHttpMethodFilter", new HiddenHttpMethodFilter());
-        registerFilter(servletContext, "multipartFilter", new MultipartFilter());
+        //// Listener Registration
+        AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
+        ac.setServletContext(servletContext);
+        ac.register(WebMvcConfiguration.class);
+        ac.refresh();
+        servletContext.addListener(new ContextLoaderListener(ac));
 
-        servletContext.addListener(new HttpSessionEventPublisher());
-        servletContext.addListener(new ContextLoaderListener(buildWebApplicationContext(servletContext, WebMvcConfiguration.class)));
-
+        //// DispatcherServlet Registration
         DispatcherServlet dispatcherServlet = new DispatcherServlet();
         dispatcherServlet.setContextClass(AnnotationConfigWebApplicationContext.class);
         ServletRegistration.Dynamic spring = servletContext.addServlet(this.springServletName, dispatcherServlet);
@@ -43,19 +41,5 @@ public class CrmWebApplicationInitializer implements WebApplicationInitializer {
 
     }
 
-    protected void registerFilter(ServletContext servletContext, String name, Filter filter) {
-        FilterRegistration.Dynamic filterRegistration = servletContext.addFilter(name, filter);
-        filterRegistration.setAsyncSupported(true);
-        filterRegistration.addMappingForUrlPatterns(null, true, this.patternAll);
-        filterRegistration.addMappingForServletNames(null, true, this.springServletName);
-    }
-
-    protected WebApplicationContext buildWebApplicationContext(ServletContext servletContext, Class... configClasses) {
-        AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
-        ac.setServletContext(servletContext);
-        ac.register(configClasses);
-        ac.refresh();
-        return ac;
-    }
 
 }
